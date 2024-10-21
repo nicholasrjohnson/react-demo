ab653d2ae52d63e9a54c4910c2c31ce1090213fc
Nicholas R Johnson <njohnson@nicholasrjohnson.com>
2024-10-21 11:43:54 -0400

    Initial Commit

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#replace this with the path of your project on the VPS
+cd ~/GIT/react-demo
+
+#pull from the branch
+git pull origin master
+
+# followed by instructions specific to your project that you used to do manually
+npm install
+export PATH=~/.npm-global/bin:$PATH
+source ~/.profile
+
+pm2 restart app
+
