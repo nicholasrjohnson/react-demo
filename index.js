469ed2fd5bbb1dbc8614cb9f7518d6324d6b7cbf
Nicholas R Johnson <njohnson@nicholasrjohnson.com>
2024-10-22 16:18:03 -0400

    making CI work

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.get("/api", (req, res) => {
-	res.json({ message: "Hello from server!" });
+	res.json({ message: "Hello from Nick!" });
 });
 
 app.listen(PORT, () => {
